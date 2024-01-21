d6f50b32e51887bee3ce7e005b4461615c80817b
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2024-01-21 20:58:29 +1100

    Update Pricing.jsx

--- my-app/src/components/Pricing.jsx
+++ my-app/src/components/Pricing.jsx
@@ -115,7 +115,7 @@ const Pricing = ({}) => {
               </li>
             </ul>
             <a
-              href="#"
+              href="https://workout.zapier.app/"
               class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
             >
               Get started
@@ -228,7 +228,7 @@ const Pricing = ({}) => {
               </li>
             </ul>
             <a
-              href="#"
+              href="https://workout.zapier.app/"
               class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
             >
               Get started
