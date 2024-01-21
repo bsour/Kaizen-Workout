e84b231d2f85fa81634640f4dc71dd109ce15f2f
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2024-01-21 21:00:55 +1100

    Update Navbar.jsx

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <header>
       <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" class="flex items-center">
+          <a href="/" class="flex items-center">
             <img
               src="/Kaizen.png"
               class="mr-3 h-6 sm:h-9"
